03ace6b93009d7b0551de009ead2114e38c8e3e0
thalodsanjeev <sanjeev.thalod@gastos.in>
2025-07-11 11:35:03 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import img from "./assets/velvet.png"
+
+function App() {
+  return (
+    <div className='body' style={{ backgroundImage: `url(${img})` }}>
+        <p className='logo'>
+          <p className='alta'>VELVET</p>
+          <p className='exp'>EXPERIENCE</p>
+        </p>
+      <div className='text'>
+
+        Comming soon
+
+      </div>
+    </div>
+  )
+}
+
+export default App
