438bb57cd8c895a458a6ad5264ad609b42cacb4b
thalodsanjeev <sanjeev.thalod@gastos.in>
2025-07-23 11:00:54 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import './App.css'
 import img from "./assets/velvet.png"
+import CarLogo from './Components/CarLogo'
+import Chauffeurs from './Components/Chaffeurs'
+import ClassApart from './Components/ClassApart'
+import Hero from './Components/Hero'
+import HowToBook from './Components/HowToBook'
 
 function App() {
   return (
-    <div className='body' style={{ backgroundImage: `url(${img})` }}>
-      <p className='logo'>
-        <p className='alta'>VELVET</p>
-        <p className='exp'>EXPERIENCE</p>
-      </p>
-      <div className='text'>
-        Launching soon
-      </div>
-      <p className='footer'>Your next Ride, Wrapped in Luxury</p>
-    </div>
+    <>
+      <Hero />
+      <CarLogo />
+      <ClassApart/>
+      <HowToBook/>
+      <Chauffeurs/>
+    </>
+    // <div className='body' style={{ backgroundImage: `url(${img})` }}>
+    //   <p className='logo'>
+    //     <p className='alta'>VELVET</p>
+    //     <p className='exp'>EXPERIENCE</p>
+    //   </p>
+    //   <div className='text'>
+    //     Launching soon
+    //   </div>
+    //   <p className='footer'>Your next Ride, Wrapped in Luxury</p>
+    // </div>
   )
 }
 
